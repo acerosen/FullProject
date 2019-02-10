455abc7e2dab3582cbaec988e0d524922cd2c6ae
acerosen <46420512+acerosen@users.noreply.github.com>
2019-02-10 17:02:37 +0200

    Add files via upload

---
+++ Logic.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BE;
+using DS;
+using System.IO;
+
+namespace ConsoleApplication
+{
+    class program
+    {
+        bool CheckAgeTrainee (Trainee T)
+{
+    if (T.Birthday.Year > 2000)
+        return false;
+    return true;
+}
+
+bool CheckAgeTester (Tester T)
+{
+    if (T.Birthday.Year > 1978)
+       return false;
+    return true;
+}
+    }
+}
+
